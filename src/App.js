0cca5a9c8396e67462785a9b6c5df24ef4bc5a5b
Diego Zaizar <diegozaizar26@gmail.com>
2019-09-03 22:48:43 -0500

    MiComponente was created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+function holamundo(nombre){
+
+  var presentacion=<h2>Hola soy {nombre}</h2>
+  return presentacion;
+}
 
 function App() {
+
+  var nombre='Diego Zaizar';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola, soy diego
         </p>
+        {holamundo(nombre)}
         <a
           className="App-link"
           href="https://reactjs.org"
